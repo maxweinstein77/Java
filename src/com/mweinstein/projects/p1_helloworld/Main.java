c4f08af86280bae81a157f0f41ccbd4814a1ea88
mweinstein7 <maxw86343@gmail.com>
2025-12-07 23:07:44 -0600

    Day 55

---
+++ src/com/mweinstein/projects/p1_helloworld/Main.java
@@ -0,0 +1,8 @@
+package com.mweinstein.projects.p1_helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/com/mweinstein/tutorials/amigoscode/javafullcourse/l1_yourfirstjavaapp/Main.java
@@ -0,0 +1,50 @@
+package com.mweinstein.tutorials.amigoscode.javafullcourse.l1_yourfirstjavaapp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+/*
+
+Java Development Kit (JDK) - environment that gives you everything you need to
+develop Java applications
+
+Java Runtime Environment (JRE) - included inside JDK; provides libraries and
+classes needed to run Java applications
+
+Java Virtual Machine (JVM) - included inside JRE; responsible for executing
+Java bytecode
+
+JDK Timeline:
+- 01/1996: JDK 1.0
+- 02/1997: JDK 1.1
+- 12/1998: JDK 1.2
+- 05/2000: JDK 1.3
+- 02/2002: JDK 1.4
+- 09/2004: JDK 5.0
+- 12/2006: JDK 6
+- 07/2011: JDK 7  (LTS)
+- 03/2014: JDK 8  (LTS)
+- 09/2017: JDK 9
+- 03/2018: JDK 10
+- 09/2018: JDK 11  (LTS)
+- 03/2019: JDK 12
+- 09/2019: JDK 13
+- 03/2020: JDK 14
+- 09/2020: JDK 15
+- 03/2021: JDK 16
+- 09/2021: JDK 17  (LTS)
+- 03/2022: JDK 18
+- 09/2022: JDK 19
+- 03/2023: JDK 20
+- 09/2023: JDK 21  (LTS)
+- 03/2024: JDK 22
+- 09/2024: JDK 23
+- 03/2025: JDK 24
+- 09/2025: JDK 25  (LTS)
+
+Since JDK 9, new version released roughly every six months.
+
+*/
