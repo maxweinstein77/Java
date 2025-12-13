a71fd3dabb242cd25b4ed91ee5055212468c44c6
mweinstein7 <maxw86343@gmail.com>
2025-12-12 22:54:17 -0600

    Day 59

---
+++ src/com/mweinstein/projects/p5_contactcard/Main.java
@@ -0,0 +1,16 @@
+package com.mweinstein.projects.p5_contactcard;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Max";
+        String lastName = "Weinstein";
+        String name = firstName + " " + lastName;
+        String email = "max@gmail.com";
+        String role = "Student";
+
+        System.out.println("Contact Card:");
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(role);
+    }
+}
