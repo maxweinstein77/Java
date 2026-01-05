45eae98a7324fe15403348dc007d4c94f4f86b17
mweinstein7 <maxw86343@gmail.com>
2026-01-04 22:33:40 -0600

    Day 82

---
+++ src/com/mweinstein/projects/p78_onethroughfive/OneThroughFive.java
@@ -0,0 +1,13 @@
+package com.mweinstein.projects.p78_onethroughfive;
+
+public class OneThroughFive {
+
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number < 6) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
