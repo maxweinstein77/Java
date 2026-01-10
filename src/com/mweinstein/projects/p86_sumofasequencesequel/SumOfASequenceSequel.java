49d7cfaf2fa6b63222023f6c6002e386eb81b6d4
mweinstein7 <maxw86343@gmail.com>
2026-01-09 21:08:14 -0600

    Day 87

---
+++ src/com/mweinstein/projects/p86_sumofasequencesequel/SumOfASequenceSequel.java
@@ -0,0 +1,24 @@
+package com.mweinstein.projects.p86_sumofasequencesequel;
+
+import java.util.Scanner;
+
+public class SumOfASequenceSequel {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First number? ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Last number? ");
+        int lastNumber = scanner.nextInt();
+
+        int sum = 0;
+
+        for (int i = firstNumber; i <= lastNumber; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
