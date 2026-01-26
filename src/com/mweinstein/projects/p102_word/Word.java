4711317437c8e42f49f15ac932b75bab2b858dac
mweinstein7 <maxw86343@gmail.com>
2026-01-25 20:26:18 -0600

    Day 103

---
+++ src/com/mweinstein/projects/p102_word/Word.java
@@ -0,0 +1,11 @@
+package com.mweinstein.projects.p102_word;
+
+public class Word {
+    public static String word() { return "word"; }
+
+    public static void main(String[] args) {
+        String word = word();
+
+        System.out.println("The method returned the word: " + word);
+    }
+}
