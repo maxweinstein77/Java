a88f367187098ed6b5e4c0ae6a1e972bcf192f2d
mweinstein7 <maxw86343@gmail.com>
2026-02-12 21:59:49 -0600

    Day 121

---
+++ src/com/mweinstein/projects/p117_hellonumber/HelloNumber.java
@@ -0,0 +1,14 @@
+package com.mweinstein.projects.p117_hellonumber;
+
+public class HelloNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        printNumber();
+        System.out.println("Bye bye world!");
+    }
+
+    public static void printNumber() {
+        System.out.println("Number");
+    }
+}
