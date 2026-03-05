f58959452697a1dae5b6dee1539e1ead95a26d07
mweinstein7 <maxw86343@gmail.com>
2026-03-04 22:34:58 -0600

    Day 141

---
+++ src/com/mweinstein/projects/p123_starsign/StarSign.java
@@ -0,0 +1,17 @@
+package com.mweinstein.projects.p123_starsign;
+
+public class StarSign {
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+    }
+}
